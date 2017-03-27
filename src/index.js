1ac057baeb9d7e36378628c1e4bef2ba3e358f72
Andrew Scheuermann <andrewscheuermann@gmail.com>
2017-03-27 11:34:14 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game/component';
+import './normalize.css';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
