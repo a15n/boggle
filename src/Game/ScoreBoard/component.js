e0987b5c91b7b2b06899f5503db2769e70fd8c18
Andrew Scheuermann <andrewscheuermann@gmail.com>
2017-03-27 18:19:15 -0700

    compute score from word.length

--- src/Game/ScoreBoard/component.js
+++ src/Game/ScoreBoard/component.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import './ScoreBoard.css';
 
 class ScoreBoard extends Component {
+  constructor() {
+    super(...arguments);
+    this.renderWordsAndScores = this.renderWordsAndScores.bind(this);
+  }
+  getPoints(l) {
+    if (l < 3) {
+      return 0;
+    } else if (l === 3) {
+      return 1;
+    } else if (l === 4) {
+      return 2;
+    } else if (l === 5) {
+      return 3;
+    } else if (l === 6) {
+      return 4;
+    } else if (l === 7) {
+      return 5;
+    } else {
+      return 6;
+    }
+  }
   renderWordsAndScores(word, i) {
     return (
       <div key={i}>
-        {word}
-        <span className="ScoreBoard-wordScore">10</span>
+        {word} <span className="ScoreBoard-wordScore">{this.getPoints(word.length)}</span>
       </div>
     )
   }
